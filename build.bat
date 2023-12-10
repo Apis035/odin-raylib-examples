7e2227c6334be4c2a954b1c427b9e28b7e7f81e9
Apis035 <gd.apis035@gmail.com>
2023-12-10 22:15:47 +0700

    add batch build script

---
+++ build.bat
@@ -0,0 +1,28 @@
+@echo off
+
+if not exist bin mkdir bin
+
+if "%1" == "clean" (
+    echo Cleaning output folder...
+    pushd bin
+    del *.exe *.bin
+    popd
+    goto :eof
+)
+
+set opt=minimal
+set flags=-disable-assert -no-bounds-check -no-thread-local
+
+if not "%1" == "" set opt=%1
+
+echo Compiling examples with optimization set to %opt%
+
+for /r %%` in (*.odin) do (
+    echo ^> %%~nx`
+    odin build %%~` -file -out:bin\%%~n`.exe -o:%opt% %flags%
+
+    :: odin setting echo back to on, bug?
+    echo off
+
+    if "%2" == "upx" upx -qqq --lzma bin\%%~n`.exe
+)
