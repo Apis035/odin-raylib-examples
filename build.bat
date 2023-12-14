a3601fb2183a4466dd3c57d6754c3f4fb50ce27f
Apis035 <gd.apis035@gmail.com>
2023-12-14 22:15:51 +0700

    improve parameter parsing using `shift` method

--- build.bat
+++ build.bat
@@ -1,28 +1,56 @@
+:: Build flags:
+::     none minimal speed size aggressive
+::     upx bin clean
+
+:: FIXME:
+:: - Odin modifying command echoing setting
+:: TODO:
+:: - Option to compile only specific example module
+:: - Help message
+
 @echo off
 
-if not exist bin mkdir bin
-
-if "%1" == "clean" (
-    echo Cleaning output folder...
-    pushd bin
-    del *.exe *.bin
-    popd
-    goto :eof
-)
-
 set opt=minimal
+set pack=
 set flags=-disable-assert -no-bounds-check -subsystem:windows
 
-if not "%1" == "" set opt=%1
 
-echo Compiling examples with optimization set to %opt%
+:ParseArgs
+    set arg=%1
+    if "%arg%" == "" goto Build
+    if "%arg%" == "clean" goto Clean
 
-for /r %%` in (*.odin) do (
-    echo ^> %%~nx`
-    odin build %%~` -file -out:bin\%%~n`.exe -o:%opt% %flags%
+    for %%A in (none, minimal, speed, size, aggressive) do if %arg% == %%A set opt=%arg%
+    if %arg% == upx set pack=1
 
-    :: odin setting echo back to on, bug?
-    echo off
+    shift
+goto ParseArgs
 
-    if "%2" == "upx" upx -qqq --lzma bin\%%~n`.exe
-)
+
+:Build
+    if defined pack (set upxmsg=true) else (set upxmsg=false)
+
+    echo.
+    echo Compiling examples...
+    echo     Optimization: %opt%
+    echo     UPX packing:  %upxmsg%
+    echo.
+
+    if not exist bin md bin
+
+    for /d %%D in (*) do (
+        for %%F in (%%D\*.odin) do (
+            echo ^> %%~nxF
+
+            odin build %%F -file -out:bin\%%~nF.exe -o:%opt% %flags%
+            echo off
+            if defined pack upx -qqq --lzma bin\%%~nF.exe
+        )
+    )
+
+    goto :eof
+
+
+:Clean
+    echo Cleaning output files...
+    del /s *.exe
